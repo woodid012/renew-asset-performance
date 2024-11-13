a894702f4a2659cc79eb04e9b2adc5f8aa137f95
woodid012 <dbwooding88@gmail.com>
2024-11-13 14:52:48 +1100

    Add PPA components with inputs and outputs integration

--- src/components/PPASummaryTable.jsx
+++ src/components/PPASummaryTable.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent } from "@/components/ui/card";
-import PPAInputs from './PPAInputs';
 import PPAOutputs from './PPAOutputs';
 
 const PPASummaryTable = () => {
   return (
-    <Tabs defaultValue="inputs" className="w-full">
+    <Tabs defaultValue="outputs" className="w-full">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-2xl font-bold">Datatable</h2>
         <TabsList>
-          <TabsTrigger value="inputs">Inputs</TabsTrigger>
           <TabsTrigger value="outputs">Outputs</TabsTrigger>
         </TabsList>
       </div>
-
-      <TabsContent value="inputs">
-        <Card>
-          <CardContent className="pt-6">
-            <PPAInputs />
-          </CardContent>
-        </Card>
-      </TabsContent>
 
       <TabsContent value="outputs">
         <Card>
